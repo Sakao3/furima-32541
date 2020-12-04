292950fa17d1b4c61c621cbfa4b2895c34d113b8
sakao3 <sakai.y0920@gmail.com>
2020-12-05 01:02:24 +0900

    アソシエーション実装
    ActiveHash５種とActiveStorageをItemモデルとアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :charge
+    belongs_to :prefecture
+    belongs_to :term
+
+    has_one_attached :image
 end
