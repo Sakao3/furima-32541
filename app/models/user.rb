1201deb77626490e973722d6804ddda10f1cb80b
sakao3 <sakai.y0920@gmail.com>
2020-12-04 11:30:31 +0900

    登録失敗時のエラーハンドリングメッセージ実装
    ユーザー登録失敗時のエラーメッセージの実装
    RSpecとpry-railsの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,19 @@ class User < ApplicationRecord
     validates :nickname
     validates :birthday
 
-    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: "Full-width characters" } do
       validates :last_name
       validates :first_name
     end
 
-    with_options format: { with: /\A[ァ-ヶ]+\z/} do
+    with_options format: { with: /\A[ァ-ヶ]+\z/ , message: "Full-width katakana characters" } do
       validates :last_name_kana
       validates :first_name_kana
     end
     
    end
+      # validates :email, presence: true
+    # validates :email,    uniqueness: { message: "Include both letters and numbers" }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: "Include both letters and numbers" }
 
-    # validates :email,    uniqueness: true
-    # validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,  }\z/i }
 end
