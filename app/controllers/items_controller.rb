e66ba2e29793fab84d9b472929cadc97ffca0751
sakao3 <sakai.y0920@gmail.com>
2020-12-06 17:00:07 +0900

    削除機能の実装
    ルーティング、コントローラー、ビューの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :item_find_id, only: [:show,:edit,:update]
+  before_action :item_find_id, only: [:show,:edit,:update,:destroy]
 
   def index
     @items = Item.order(id: "DESC")
@@ -34,6 +34,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to action: :index
   end
 
 
