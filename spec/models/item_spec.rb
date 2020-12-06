d7b26207ef26a736b34f865bd52a0d2531e8f355
sakao3 <sakai.y0920@gmail.com>
2020-12-06 10:40:53 +0900

    商品出品機能　修正１
    RSpecの一部修正いたしました

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,27 +38,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Introduction can't be blank"
       end
       it "category_id[1]を選択している場合" do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Category Select"
       end
       it "status_id[1]を選択ている場合" do
-        @item.status_id = "1"
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Status Select"
       end
       it "charge_id[1]を選択している場合" do
-        @item.charge_id = "1"
+        @item.charge_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Charge Select"
       end
       it "prefecture_id[1]を選択している場合" do
-        @item.prefecture_id = "1"
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Prefecture Select"
       end
       it "term_id[1]を選択している場合" do
-        @item.term_id = "1"
+        @item.term_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Term Select"
       end
