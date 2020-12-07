cacf0a4f3ebc272a6eb5123a0dfca4de5f95c1e5
sakao3 <sakai.y0920@gmail.com>
2020-12-07 17:08:47 +0900

    addressモデル作成
    ・addressのマイグレーションファイルに必要なカラム追加
    ・Ordersモデル、ActiveHashとのアソシエーション設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,6 @@ class OrdersController < ApplicationController
   def index
     @order=Order.new
     @item=Item.find(params[:item_id])
-    
     if user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
