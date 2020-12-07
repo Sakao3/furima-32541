cac49223507ec3a89ae01683cc2cdc7b3363c1f0
sakao3 <sakai.y0920@gmail.com>
2020-12-07 22:47:50 +0900

    正規表現の修正、購入時のアクション
    正規表現修正、確認済
    購入時、成功と失敗で画面の変化
    成功時はリダイレクト、失敗時はレンダーで同じ画面（情報が残ったまま）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,38 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :item_find
 
   def index
-    @order=Order.new
-    @item=Item.find(params[:item_id])
+    @item_order=ItemOrder.new
     if user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
   end
 
   def create
+    @item_order=ItemOrder.new(item_order_params)
+    if @item_order.valid?
+      @item_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def item_order_params
+    params.require(:item_order).permit(
+      :postal_code, :prefecture_id, :city, :block, :building, :telephone_number
+    ).merge(user_id: current_user.id ,item_id: params[:item_id])
+  end
+
+  def item_find
+    @item=Item.find(params[:item_id])
   end
 
 end
+
+# @を付けて誤情報を送り、elseをrenderにしていると、エラーメッセージは出るが、情報が残る
+
+# @を外して誤情報を送り、elseをredirectにしているとエラーメッセージはでないが情報は残らない
+# →＠なしのrenderはビューにインスタンス変数を入れているためエラーが出る
--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -4,25 +4,25 @@ class ItemOrder
   # アクティブハッシュ用に一応記載、アソシエーション用のため、addressに記載しているから多分いらない気がする
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :telephone_number
+  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :telephone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code,   format: {with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :postal_code,   format: {with: /\A\d{3}[-]\d{4}\z/ ,message: "Input correctly" }
+    validates :prefecture_id, numericality: { other_than: 1 ,message: "Select"}
     validates :city
     validates :block
-    validates :telephone_number, format: { with: /\A[０-９\d]{ ,11}\z/ }, length: { muximum: 11 }
+    validates :telephone_number, format: { with: /\A[0-9,０-９]+\z/ ,message: "Input only number" }, 
+                                 length: {maximum: 11 }
   end
 
   def save
-    item=Item.find(params[:item_id])
 
-    order=Order.create(item_id: item.id, user_id: item.user)
+    order=Order.create(item_id: item_id, user_id: user_id)
 
     Address.create(
       postal_code: postal_code, prefecture_id: prefecture_id, city: city, 
-      block: block, telephone_number: telephone_number, order_id: order.id
+      block: block, building: building,  telephone_number: telephone_number, order_id: order.id
     )
   end
-  
+
 end
