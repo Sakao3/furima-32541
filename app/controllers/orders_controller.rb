41a95e384ff2b5e2aa7e4114bab9fbc544f03019
sakao3 <sakai.y0920@gmail.com>
2020-12-07 16:55:09 +0900

    強制ページ遷移
    未ログインユーザーが購入ページに直アクセス→ログインページへ
    ログインユーザーが自分の出品したページに直アクセス→商品一覧ページへ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @order=Order.new
-
     @item=Item.find(params[:item_id])
-
+    
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def create
