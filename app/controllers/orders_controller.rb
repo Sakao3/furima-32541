a8aa0273e00e58f1ce30c447ab8c293bc6f21042
sakao3 <sakai.y0920@gmail.com>
2020-12-08 10:44:43 +0900

    クレジット処理完了
    クレジット処理完了
    環境変数設定済

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,6 +12,9 @@ class OrdersController < ApplicationController
   def create
     @item_order=ItemOrder.new(item_order_params)
     if @item_order.valid?
+
+      pay_item
+
       @item_order.save
       redirect_to root_path
     else
@@ -23,12 +26,23 @@ class OrdersController < ApplicationController
   def item_order_params
     params.require(:item_order).permit(
       :postal_code, :prefecture_id, :city, :block, :building, :telephone_number
-    ).merge(user_id: current_user.id ,item_id: params[:item_id])
+    ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def item_find
     @item=Item.find(params[:item_id])
   end
+
+
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      card: item_order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 
 end
 
--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -4,7 +4,7 @@ class ItemOrder
   # アクティブハッシュ用に一応記載、アソシエーション用のため、addressに記載しているから多分いらない気がする
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :telephone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :telephone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code,   format: {with: /\A\d{3}[-]\d{4}\z/ ,message: "Input correctly" }
@@ -13,6 +13,9 @@ class ItemOrder
     validates :block
     validates :telephone_number, format: { with: /\A[0-9,０-９]+\z/ ,message: "Input only number" }, 
                                  length: {maximum: 11 }
+
+    validates :token
+    
   end
 
   def save
